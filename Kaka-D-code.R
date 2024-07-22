0945434b4e7524b2cf0848360efa4452f14b676b
DerekWongg <161673019+DerekWongg@users.noreply.github.com>
2024-07-22 13:34:26 +1200

    Update Kaka-D-code.R
    Write new functions for cleaner codes,
    made the install.package call optional,
    added ggsave at the end to export the plot

--- Kaka-D-code.R
+++ Kaka-D-code.R
@@ -1,124 +1,97 @@
 library(tidyverse)
-install.packages("ggwordcloud")
-install.packages("wordcloud2")
+# Use the following install call only if you haven't installed these packages
+## install.packages("ggwordcloud")
+## install.packages("wordcloud2")
 library(wordcloud2)
 library(ggwordcloud)
 
+# Read and prepare data
 data <- read_csv("sentiment-survey-data.csv")
 names(data) <- c("four_words", "majority", "instructor")
 data <- data %>% mutate(id = row_number())
 
-
-
-# separate the 4 words
-data <- data %>% separate_rows(four_words, sep = " , ") %>%
-  separate_rows(four_words, sep = ", ") %>%
-  separate_rows(four_words, sep = ",") %>%
-  separate_rows(four_words, sep = "，")
-
-# clean the four_words column (lower cases and take out punctuation and digit)
+# clean_col function tranform the four seperated words into lower case and take out punctuations and numbers
 clean_col <- function(word) {
   str_to_lower(word) %>%
     str_remove_all("[[:punct:]]") %>%
     str_remove_all("[[:digit:]]")
 }
 
-data["four_words"] <- lapply(data["four_words"], function(word) clean_col(word))
+# separate_and_clean function seperates one four_words cell into 4 seperated row and clean, and take out invalid format of response
+separate_and_clean <- function(data, column) {
+  data %>%
+    separate_rows({{column}}, sep = " , |, |,|，") %>%
+    mutate({{column}} := clean_col({{column}})) %>%
+    filter(!str_count({{column}}, "\\S+") > 1 & nchar({{column}}) > 0)
+}
 
-# take out rows with more than one word in four_words
-data <- data %>% filter(!str_count(four_words, "\\S+") > 1) %>%
-  filter(!nchar(four_words) == 0)
+# join_sentiment function join the words column with sentiment_list, and give "column_sentiment" accordingly
+join_sentiment <- function(data, column, sentiment_list) {
+  data %>%
+    inner_join(sentiment_list, by = setNames("word", column)) %>%
+    rename_with(~ paste0(column, "_sentiment"), sentiment)
+}
 
-# Create a column that counts the frequency of words in the four_words column
-data <- data %>% 
+# Apply separate_and_clean to four_words, majority, and instructor
+data <- separate_and_clean(data, four_words)
+data <- separate_and_clean(data, majority)
+data <- separate_and_clean(data, instructor)
+
+# Count frequency of words in four_words
+data <- data %>%
   group_by(four_words) %>%
   mutate(frequency = n()) %>%
-  ungroup()
+  ungroup() %>%
+  distinct(four_words, .keep_all = TRUE)
 
-# clean the second row
-data["majority"] <- lapply(data["majority"], function(word) clean_col(word))
-data <- data %>% filter(!str_count(majority, "\\S+") > 1) %>%
-  filter(!nchar(majority) == 0)
-
-# clean the third row
-data["instructor"] <- lapply(data["instructor"], function(word) clean_col(word))
-data <- data %>% filter(!str_count(instructor, "\\S+") > 1) %>%
-  filter(!nchar(majority) == 0)
-
-# Take out repeated words
-used_words <- data$four_words[1]
-repeated <- FALSE
-for (i in 2:length(data$four_words)) {
-  if(data$four_words[i] %in% used_words) {
-    repeated <- c(repeated, TRUE)} 
-  else {
-    repeated <- c(repeated, FALSE)
-    used_words <- c(used_words, data$four_words[i])
-  }
-}
-data$repeated_word <- repeated
-non_repeated_words <- data %>% filter(!repeated_word)
+# Remove repeated words
+data <- data %>%
+  filter(!duplicated(four_words))
 
 # Read the sentiment list
 sentiment_list <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vR6jVuO3F3DNwX1WApTvCfYqfjehcNKHmuDqupk2_0vJe0lnf81dmUlsXZGkZKmaCeallS5Dqch05ks/pub?gid=422750759&single=true&output=csv")
 
-#Put id col in front
-non_repeated_words <- non_repeated_words %>% relocate(id) 
-non_repeated_words <- non_repeated_words %>% relocate(frequency, .after = four_words)
+# Apply join_sentiment for four_words
+data <- join_sentiment(data, "four_words", sentiment_list)
 
-# Join four_words with sentiment list
-non_repeated_words <- inner_join(non_repeated_words, sentiment_list, by = c("four_words" = "word"))
-non_repeated_words <- non_repeated_words %>% relocate(sentiment, .after = four_words)
-non_repeated_words <- non_repeated_words %>% rename(four_word_sentiment = 3)
-four_words_data <- non_repeated_words %>% select(four_words, frequency, four_word_sentiment) %>% arrange(desc(frequency))
+# Export the cleaned four words sentiment data as csv
+write_csv(data, "cleaned_sentiment_data.csv")
 
-# Join majority with sentiment list
-non_repeated_words <- inner_join(non_repeated_words, sentiment_list, by = c("majority" = "word")) #Join majority
-non_repeated_words <- non_repeated_words %>% relocate(sentiment, .after = majority)
-non_repeated_words <- non_repeated_words %>% rename(majority_sentiment = 6)
+# Apply join_sentiment for majority and instructor
+data <- join_sentiment(data, "majority", sentiment_list)
+data <- join_sentiment(data, "instructor", sentiment_list)
 
-# Join instructor with sentiment list
-non_repeated_words <- inner_join(non_repeated_words, sentiment_list, by = c("instructor" = "word")) #Join instructor
-non_repeated_words <- non_repeated_words %>% relocate(sentiment, .after = instructor)
-non_repeated_words <- non_repeated_words %>% rename(finstructor_sentiment = 8)
-
-#Take out the repeated_words col
-non_repeated_words <- non_repeated_words[1:8] 
-non_repeated_words <- non_repeated_words %>% arrange(desc(frequency))
-
-# Plot the graph with the four_words_data solely
-wordcloud2(four_words_data, size = 2, color = "random-light", backgroundColor = "grey")
-
-
-
+# Reorder columns
+data <- data %>%
+  select(id, four_words, four_words_sentiment, frequency, majority, majority_sentiment, instructor, instructor_sentiment) %>%
+  arrange(desc(frequency))
 
 #################################################################################
+# Read the saved cleaned sentiment data csv
+four_words_data <- read_csv("cleaned_sentiment_data.csv")
+
 # Apply logarithmic transformation to frequencies
 wordcloud_data <- four_words_data %>% 
   mutate(log_freq = log1p(frequency))
 
 # Select the necessary columns for the word cloud
 wordcloud_data <- wordcloud_data %>% 
-  select(four_words, log_freq, four_word_sentiment)
+  select(four_words, log_freq, four_words_sentiment)
 
-# Generate the word cloud
-wordcloud2(wordcloud_data, size = 0.5, color = 'random-light', backgroundColor = "maroon", shape = 'circle')
 #################################################################################
 
 # ggplot version of the word cloud with different colours for negative/positive
-## Edit the theme and code if you want something different, I don't mind. 
-### Also feels like we need another graph to supplement this lol.
 
-ggplot(wordcloud_data, aes(label = four_words, size = frequency, color = four_word_sentiment,
+ggplot(wordcloud_data, aes(label = four_words, size = log_freq, color = four_words_sentiment,
                            fontface = "bold")) +
   geom_text_wordcloud(shape = "square", show.legend = TRUE) +
   scale_size_area(max_size = 30)+
   scale_color_manual(values = c("positive" = "chartreuse2", "negative" = "red"),
-    labels = c("positive", "Negative"), 
-    name = "Sentiment Among Students") +
+                     labels = c("positive", "Negative"), 
+                     name = "Sentiment Among Students") +
   labs(title = "Sentiment Word Cloud",
-    subtitle = "A visualisation of the sentiment of respondents to four-word phrases ",
-    caption = "Data source: Stats399 Students") +
+       subtitle = "A visualisation of the sentiment of respondents to four-word phrases ",
+       caption = "Data source: Stats399 Students") +
   theme(
     panel.background = element_rect(fill = 'aliceblue'),
     plot.background = element_rect(fill = "aliceblue"),
@@ -134,9 +107,5 @@ ggplot(wordcloud_data, aes(label = four_words, size = frequency, color = four_wo
   guides(size = "none",
          color = guide_legend(nrow = 1))
 
-
-
-
-
-
-
+# Export the plot with wider dimension to contain all words
+ggsave("sentiment_wordcloud.png", plot = last_plot(), width = 15, height = 10, dpi = 480)
